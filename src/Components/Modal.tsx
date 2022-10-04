4ed7e2a056b131bc3c9b8bc6636acd2fcb7ce69c
Hatem Alimam <me@hatemalimam.com>
2022-10-04 10:00:26 +0300

    added ContainerProps

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -72,7 +72,7 @@ export class ModalComponent extends React.PureComponent<IModalProps, IModalState
 	}
 
 	public render(): JSX.Element {
-		const { autoSort, modalAnimationType, onClosed, showAlphabeticalIndex, searchInputTextColor, keyExtractor, showToTopButton, onEndReached, removeClippedSubviews, FlatListProps, selectPlaceholderText, searchPlaceholderText, SearchInputProps, selected, disabled, items, requireSelection, renderSelectView, ModalProps, backButtonDisabled, renderSearch } = this.props;
+		const { autoSort, modalAnimationType, onClosed, showAlphabeticalIndex, searchInputTextColor, keyExtractor, showToTopButton, onEndReached, removeClippedSubviews, FlatListProps, selectPlaceholderText, searchPlaceholderText, SearchInputProps, selected, disabled, items, requireSelection, renderSelectView, ModalProps, ContainerProps, backButtonDisabled, renderSearch } = this.props;
 
 		const { modalVisible, alphabeticalIndexChars, stickyBottomButton, selectedAlpha, selectedObject, searchText } = this.state;
 
@@ -92,7 +92,7 @@ export class ModalComponent extends React.PureComponent<IModalProps, IModalState
 					onRequestClose={() => onClosed}
 					{...ModalProps}
 				>
-					<SafeAreaView style={ModalStyles.container}>
+					<SafeAreaView style={ModalStyles.container} {...ContainerProps}>
 						{
 							renderSearch ? renderSearch(
 									this.onClose.bind(this), 
